918ce3852288e8f004d09b5fd0106aa9c983582f
CongDC <dinhchicongf9@gmail.com>
2023-06-12 16:19:58 +0700

    fix some

--- systems/bnerf.py
+++ systems/bnerf.py
@@ -55,7 +55,7 @@ class BNeRFSystem(BaseSystem):
             elif self.dataset.directions.ndim == 4: # (N, H, W, 3) 
                 directions = self.dataset.directions[index, y, x]
 
-            print(f"c2w {c2w}")
+            print(f"c2w {c2w.shape}")
             rays_o, rays_d = get_rays(directions, c2w) # Khởi tạo tia [8192,3], [8192,3]
             rgb = self.dataset.all_images[index, y, x].view(-1, self.dataset.all_images.shape[-1]).to(self.rank) # Khởi tạo nhãn
             fg_mask = self.dataset.all_fg_masks[index, y, x].view(-1).to(self.rank)
