386a56ac6dc01e054591a0911b1195235f87c502
CongDC <dinhchicongf9@gmail.com>
2023-06-12 22:23:19 +0700

    fix some

--- models/neus.py
+++ models/neus.py
@@ -176,7 +176,7 @@ class NeuSModel(BaseModel):
         intervals = t_ends - t_starts
 
         density, feature = self.geometry_bg(positions) 
-        rgb = self.texture_bg(feature, t_dirs)
+        rgb, _ = self.texture_bg(feature, t_dirs)
 
         weights = render_weight_from_density(t_starts, t_ends, density[...,None], ray_indices=ray_indices, n_rays=n_rays)
         opacity = accumulate_along_rays(weights, ray_indices, values=None, n_rays=n_rays)
@@ -232,7 +232,7 @@ class NeuSModel(BaseModel):
             sdf, sdf_grad, feature = self.geometry(positions, with_grad=True, with_feature=True)
         normal = F.normalize(sdf_grad, p=2, dim=-1)
         alpha = self.get_alpha(sdf, normal, t_dirs, dists)[...,None]
-        rgb = self.texture(feature, t_dirs, normal)
+        rgb, _ = self.texture(feature, t_dirs, normal)
 
         weights = render_weight_from_alpha(alpha, ray_indices=ray_indices, n_rays=n_rays)
         opacity = accumulate_along_rays(weights, ray_indices, values=None, n_rays=n_rays)
@@ -316,6 +316,6 @@ class NeuSModel(BaseModel):
         if export_config.export_vertex_color:
             _, sdf_grad, feature = chunk_batch(self.geometry, export_config.chunk_size, False, mesh['v_pos'].to(self.rank), with_grad=True, with_feature=True)
             normal = F.normalize(sdf_grad, p=2, dim=-1)
-            rgb = self.texture(feature, -normal, normal) # set the viewing directions to the normal to get "albedo"
+            rgb, _ = self.texture(feature, -normal, normal) # set the viewing directions to the normal to get "albedo"
             mesh['v_rgb'] = rgb.cpu()
         return mesh
