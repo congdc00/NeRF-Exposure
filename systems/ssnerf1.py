710075021d0c3d5956e45cd2ed7efc25c89fdef6
congdinhchi <dinhchicongf9@gmail.com>
2023-06-29 17:01:55 +0700

    fix some

--- systems/ssnerf1.py
+++ systems/ssnerf1.py
@@ -135,10 +135,10 @@ class SSNeRF1System(BaseSystem):
         
         torch.save(out['theta'], "theta.pt")
         torch.save(out['positions'], "positions.pt")
-        
+        print("save Images")
         self.save_image_grid(f"it{self.global_step}-{batch['index'][0].item()}.png", [
             {'type': 'rgb', 'img': batch['rgb'].view(H, W, 3), 'kwargs': {'data_format': 'HWC'}},
-            {'type': 'rgb', 'img': out['comp_rgb'].view(H, W, 3), 'kwargs': {'data_format': 'HWC'}},
+            # {'type': 'rgb', 'img': out['comp_rgb'].view(H, W, 3), 'kwargs': {'data_format': 'HWC'}},
             {'type': 'rgb', 'img': out['real_rgb'].view(H, W, 3), 'kwargs': {'data_format': 'HWC'}},
             {'type': 'grayscale', 'img': out['depth'].view(H, W), 'kwargs': {}}
         ])
