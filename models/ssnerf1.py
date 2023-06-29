d5e6ef6da508416df029ee03094702ef35cc326d
congdinhchi <dinhchicongf9@gmail.com>
2023-06-29 14:10:47 +0700

    fix some

--- models/ssnerf1.py
+++ models/ssnerf1.py
@@ -112,11 +112,15 @@ class SSNeRF1Model(BaseModel):
         opacity = accumulate_along_rays(weights, ray_indices, values=None, n_rays=n_rays)
         # Màu sắc dự đoán ra
         comp_rgb = accumulate_along_rays(weights, ray_indices, values=rgb, n_rays=n_rays) #([Num_points, 1])
+        print(f"comp_rgb 1 {comp_rgb.size()}")
         # Độ sáng
         bright_ness = accumulate_along_rays(weights, ray_indices, values=bright_ness, n_rays=n_rays)
+        
 
         comp_rgb = comp_rgb*bright_ness
+        print(f"comp_rgb 2 {comp_rgb.size()}")
         comp_rgb = comp_rgb + self.background_color * (1.0 - opacity)   
+        print(f"comp_rgb 3 {comp_rgb.size()}")
 
         depth = accumulate_along_rays(weights, ray_indices, values=midpoints, n_rays=n_rays)    
 
