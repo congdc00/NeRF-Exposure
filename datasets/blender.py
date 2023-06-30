ab4d44ebbc9e81e650df4cd3337ffe2d6c6a8931
congdinhchi <dinhchicongf9@gmail.com>
2023-06-30 16:42:04 +0700

    fix some

--- datasets/blender.py
+++ datasets/blender.py
@@ -59,11 +59,12 @@ class BlenderDatasetBase():
 
             img_path = os.path.join(self.config.root_dir, f"{frame['file_path']}.exr")
             image = cv2.imread(img_path, cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)
-            img = Image.fromarray(np.float32(image))
+            img = image
+            # img = Image.fromarray(np.float32(image))
 
-            #img = Image.open(img_path)
+            # #img = Image.open(img_path)
 
-            img = img.resize(self.img_wh, Image.BICUBIC)
+            # img = img.resize(self.img_wh, Image.BICUBIC)
             img = TF.to_tensor(img).permute(1, 2, 0) # (4, h, w) => (h, w, 4)
 
             self.all_fg_masks.append(img[..., -1]) # (h, w)
