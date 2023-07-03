e4bc6f052c676821312a729028bae4fd236c9782
congdinhchi <dinhchicongf9@gmail.com>
2023-07-03 16:06:22 +0700

    fix some

--- models/ssnerf1.py
+++ models/ssnerf1.py
@@ -120,7 +120,6 @@ class SSNeRF1Model(BaseModel):
         comp_rgb = real_rgb*bright_ness
 
         comp_rgb = comp_rgb + self.background_color * (1.0 - opacity) 
-        real_rgb = real_rgb + self.background_color * (1.0 - opacity) 
 
         depth = accumulate_along_rays(weights, ray_indices, values=midpoints, n_rays=n_rays)    
 
