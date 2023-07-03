fae1eb957c6dbec87ee59ba21b51001c3f1b9c49
CongDC <dinhchicongf9@gmail.com>
2023-07-04 00:11:45 +0700

    fix some

--- models/ssnerf1.py
+++ models/ssnerf1.py
@@ -117,7 +117,7 @@ class SSNeRF1Model(BaseModel):
         # Độ sáng
         weight_f = torch.ones_like(weights)
         bright_ness = accumulate_along_rays(weight_f, ray_indices, values=bright_ness_out, n_rays=n_rays)
-        print(f"type bright_ness_out ---------------------- {bright_ness_out.size()}")
+        print(f"type bright_ness_out ---------------------- {bright_ness_out[0]}")
         print(f"type bright_ness ---------------------- {bright_ness.size()}")
         print(f"type real_rgb ---------------------- {real_rgb.size()}")
         comp_rgb = real_rgb*bright_ness
