1673e0546a6518383e892e38fd81af2d91883c49
congdinhchi <dinhchicongf9@gmail.com>
2023-07-06 14:57:34 +0700

    :

--- systems/ssnerf3.py
+++ systems/ssnerf3.py
@@ -42,8 +42,8 @@ class SSNeRF3System(BaseSystem):
             else:
                 index = torch.randint(0, len(self.dataset.all_images), size=(1,), device=self.dataset.all_images.device)
         
-        epoch += 1
-        print(f"epoch {epoch}")
+        SSNeRF3System.epoch += 1
+        print(f"epoch {SSNeRF3System.epoch}")
 
         if stage in ['train']:
             # bright_ness = []
