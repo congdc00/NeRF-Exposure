fd334f5f59800fa0f40d6014c5ecb233984ce922
congdinhchi <dinhchicongf9@gmail.com>
2023-07-06 09:46:34 +0700

    :

--- models/ssnerf1.py
+++ models/ssnerf1.py
@@ -129,6 +129,8 @@ class SSNeRF1Model(BaseModel):
         #Độ sáng
         comp_rgb = comp_rgb + self.background_color * (1.0 - opacity) 
         real_rgb = real_rgb + self.background_color * (1.0 - opacity) 
+
+        print(f"-------------{comp_rgb[12]}----------")
         print(f"-------------{bright_ness[12]}----------")
         
         # Export 
