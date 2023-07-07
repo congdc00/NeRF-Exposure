2ea757e89c5193e7c51e9264c5dff7758fcac8e5
congdinhchi <dinhchicongf9@gmail.com>
2023-07-07 15:51:02 +0700

    :

--- models/ssnerf1.py
+++ models/ssnerf1.py
@@ -127,7 +127,7 @@ class SSNeRF1Model(BaseModel):
 
         print(f"\n rgb {rgb[0].tolist()}")
         print(f"\n bright ness {bright_ness[0].item()}")
-        print(f"\n comp_rgb {comp_rgb[0].item()}")
+        print(f"\n comp_rgb {comp_rgb[0].tolist()}")
         print(f"\n new_rgb {new_rgb[0].tolist()}")
 
         # Export 
