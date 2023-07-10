168b761b6881e02f3ab4608d51c72b3fb082fdc2
CongDC <dinhchicongf9@gmail.com>
2023-07-11 03:40:36 +0700

    :

--- models/ssnerf1.py
+++ models/ssnerf1.py
@@ -120,7 +120,8 @@ class SSNeRF1Model(BaseModel):
         weights = render_weight_from_density(t_starts, t_ends, density[...,None], ray_indices=ray_indices, n_rays=n_rays) #([Num_points, 1])
         opacity = accumulate_along_rays(weights, ray_indices, values=None, n_rays=n_rays)
 
-        t_origins_camera =  accumulate_along_rays(weights, ray_indices, values=t_origins, n_rays=n_rays)
+        tmp = torch.ones_like(weights)
+        t_origins_camera =  accumulate_along_rays(tmp, ray_indices, values=t_origins, n_rays=n_rays)
         # print(f"t_origins_camera {t_origins_camera}")
         # print(f"t_origins {t_origins}")
         bright_ness = self.shutter_speed(True, t_origins_camera)
