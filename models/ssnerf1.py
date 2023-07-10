8b0593c8a16b44ff21aaef19853bcab38eae389c
congdinhchi <dinhchicongf9@gmail.com>
2023-07-10 14:08:55 +0700

    :

--- models/ssnerf1.py
+++ models/ssnerf1.py
@@ -143,35 +143,35 @@ class SSNeRF1Model(BaseModel):
         if self.training:
             
             # for check
-            file_path = f"./log_epoch_3.txt"
-            content = []
-            headers = ["brightness", "rgb", "rgb*brightness","weights","volume_rendering rgb", "volume_rendering rgb*brightness"]
-            k = 0
-            bright_ness_old = bright_ness[0].item()
-            with open(file_path, 'w') as file:
-                for i in range (bright_ness.shape[0]):
+            # file_path = f"./log_epoch_3.txt"
+            # content = []
+            # headers = ["brightness", "rgb", "rgb*brightness","weights","volume_rendering rgb", "volume_rendering rgb*brightness"]
+            # k = 0
+            # bright_ness_old = bright_ness[0].item()
+            # with open(file_path, 'w') as file:
+            #     for i in range (bright_ness.shape[0]):
 
-                    # brightness
-                    number = bright_ness[i].item()
-                    content_line = []
-                    content_line.append(number)
-                    content_line.append(rgb[i].tolist())
-                    content_line.append(new_rgb[i].tolist())
-                    content_line.append(weights[i].item())
+            #         # brightness
+            #         number = bright_ness[i].item()
+            #         content_line = []
+            #         content_line.append(number)
+            #         content_line.append(rgb[i].tolist())
+            #         content_line.append(new_rgb[i].tolist())
+            #         content_line.append(weights[i].item())
                     
                     
 
-                    if number != bright_ness_old and k < real_rgb.shape[0] -1:
-                        k+=1
-                        bright_ness_old = number
+            #         if number != bright_ness_old and k < real_rgb.shape[0] -1:
+            #             k+=1
+            #             bright_ness_old = number
 
-                    # content_line.append(new_brightness[k].tolist())
-                    content_line.append(real_rgb_out[k].tolist())
-                    content_line.append(comp_rgb_out[k].tolist())
-                    content.append(content_line)
+            #         # content_line.append(new_brightness[k].tolist())
+            #         content_line.append(real_rgb_out[k].tolist())
+            #         content_line.append(comp_rgb_out[k].tolist())
+            #         content.append(content_line)
                 
-                table = tabulate(content, headers, tablefmt="grid")
-                file.write(table)
+            #     table = tabulate(content, headers, tablefmt="grid")
+            #     file.write(table)
 
             out.update({
                 'weights': weights.view(-1),
