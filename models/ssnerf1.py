9a84f3b302e3960c46083dd52402811678aada58
congdinhchi <dinhchicongf9@gmail.com>
2023-07-10 10:52:27 +0700

    :

--- models/ssnerf1.py
+++ models/ssnerf1.py
@@ -140,7 +140,7 @@ class SSNeRF1Model(BaseModel):
         
         if self.training:
             # for check
-            file_path = f"./log_epoch_1.txt"
+            file_path = f"./log_epoch_2.txt"
             content = []
             headers = ["brightness", "rgb", "rgb*brightness","weights", "volume_rendering rgb", "volume_rendering rgb*brightness"]
             k = 0
