7582161631df02119d373a89554a9c5c86c6121a
congdinhchi <dinhchicongf9@gmail.com>
2023-07-18 16:59:53 +0700

    :

--- models/ssnerf1.py
+++ models/ssnerf1.py
@@ -121,6 +121,7 @@ class SSNeRF1Model(BaseModel):
         # Export 
         out = {
             'comp_rgb': comp_rgb,
+            'bright_ness':bright_ness,
             "real_rgb": real_rgb,
             'opacity': opacity,
             'depth': depth,
