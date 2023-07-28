e5443ab137cff263e79976d3a8915b3da14ef5c1
CongDC <dinhchicongf9@gmail.com>
2023-07-28 23:07:52 +0700

    :

--- launch.py
+++ launch.py
@@ -90,7 +90,7 @@ def main():
         loggers += [TensorBoardLogger(args.runs_dir, name=config.name, version=config.trial_name),CSVLogger(config.exp_dir, name=config.trial_name, version='csv_logs')]
     
     
-    strategy = 'ddp_find_unused_parameters_false'
+    strategy = 'ddp_find_unused_parameters_true'
     
     # Step 5: Bắt đầu train
     trainer = Trainer(
