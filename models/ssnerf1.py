7192aac2cc960ff6bfbdf2dc1780017027f0ce31
CongDC <dinhchicongf9@gmail.com>
2023-07-28 22:45:00 +0700

    :

--- models/ssnerf1.py
+++ models/ssnerf1.py
@@ -99,8 +99,6 @@ class SSNeRF1Model(BaseModel):
 
         density, cor_feature = self.geometry(positions) # Dự đoán mật độ thể tích => density [N_rays];cor_feature [N_rays, 16]16 là số chiều được mã hoá ra
         rgb = self.texture(True, cor_feature, t_dirs) # Dự đoán ra màu sắc
-
-        print(f"self.shutter_speed {self.shutter_speed.requires_grad_}")
         bright_ness = self.shutter_speed(True, rays_o) * 2
             
 
