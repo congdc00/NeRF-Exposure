47906574d46fe7be6789b1618765e11b9c44c2f9
CongDC <dinhchicongf9@gmail.com>
2023-07-28 16:59:49 +0700

    :

--- systems/ssnerf1.py
+++ systems/ssnerf1.py
@@ -33,6 +33,7 @@ class SSNeRF1System(BaseSystem):
     def prepare(self):
         self.criterions = {
             'psnr': PSNR()
+            'ssim': SSIM()
         }
         self.train_num_samples = self.config.model.train_num_rays * self.config.model.num_samples_per_ray
         self.train_num_rays = self.config.model.train_num_rays
