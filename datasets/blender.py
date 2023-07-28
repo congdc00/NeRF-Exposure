75519bfa045af252cfef85d7924e4ab47ed0c03a
CongDC <dinhchicongf9@gmail.com>
2023-07-28 15:31:02 +0700

    :

--- datasets/blender.py
+++ datasets/blender.py
@@ -77,7 +77,7 @@ class BlenderDatasetBase():
                 img.convert("F")
                 enhancer = ImageEnhance.Brightness(img)
                 img = enhancer.enhance(exposure_factor)
-                print(f"exposure_factor {exposure_factor}")
+                print(f"exposure_factor {type(exposure_factor)}")
                 self.all_factor.append(exposure_factor)
             except:
                 self.all_factor.append(float(1))
