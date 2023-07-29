34af108e050a1689490dd9823fd73d707cb8e3e2
CongDC <dinhchicongf9@gmail.com>
2023-07-29 11:16:14 +0700

    :

--- models/eneus.py
+++ models/eneus.py
@@ -249,7 +249,7 @@ class ENeuSModel(BaseModel):
         normal = F.normalize(sdf_grad, p=2, dim=-1)
         alpha = self.get_alpha(sdf, normal, t_dirs, dists)[...,None]
         rgb = self.texture(True, feature, t_dirs, normal)
-        bright_ness = self.shutter_speed(True, rays_o, normal) * 2
+        bright_ness = self.shutter_speed(True, rays_o) * 2
         
         weights = render_weight_from_alpha(alpha, ray_indices=ray_indices, n_rays=n_rays)
         opacity = accumulate_along_rays(weights, ray_indices, values=None, n_rays=n_rays)
