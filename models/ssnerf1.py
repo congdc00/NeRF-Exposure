197eb5c91603d50859f4e320aed909625ecddea4
CongDC <dinhchicongf9@gmail.com>
2023-07-30 00:37:49 +0700

    fix

--- models/ssnerf1.py
+++ models/ssnerf1.py
@@ -103,7 +103,7 @@ class SSNeRF1Model(BaseModel):
         self.iterator += 1
         if self.iterator % 500 == 0:
             self.is_freeze = not self.is_freeze
-            print(f" Doi ca ")
+            
         rgb = self.texture(self.is_freeze, cor_feature, t_dirs) # Dự đoán ra màu sắc
         bright_ness = self.shutter_speed(not self.is_freeze, rays_o) * 2
             
