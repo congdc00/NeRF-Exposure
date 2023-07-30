1f46dabb607db2b617355071f8f0be211f50f842
CongDC <dinhchicongf9@gmail.com>
2023-07-30 16:13:52 +0700

    :

--- systems/ssnerf1.py
+++ systems/ssnerf1.py
@@ -135,8 +135,8 @@ class SSNeRF1System(BaseSystem):
 
         ex_delta = torch.mean(ex_delta_matrix)
         k = 0.01
-        total_loss = loss_rgb + k*ex_delta
-        total_loss = total_loss/2
+        total_loss = loss_rgb*0.5 + k*ex_delta*0.25
+
         self.log('train/loss_rgb', total_loss)
         loss += total_loss * self.C(self.config.system.loss.lambda_rgb)
 
