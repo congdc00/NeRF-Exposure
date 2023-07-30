9fe6d8a7f8754aeaa67249aa59026cb793b3ca65
CongDC <dinhchicongf9@gmail.com>
2023-07-30 17:10:35 +0700

    :

--- models/ssnerf1.py
+++ models/ssnerf1.py
@@ -102,7 +102,11 @@ class SSNeRF1Model(BaseModel):
         
         # self.iterator += 1
         # if self.iterator % 500 == 0:
-        self.is_freeze = not self.is_freeze
+        self.iterator += 1
+        if self.iterator % 5000 == 0:
+            self.is_freeze = not self.is_freeze
+        if self.iterator > 10000: 
+            self.is_freeze = not self.is_freeze
 
         rgb = self.texture(self.is_freeze, cor_feature, t_dirs) # Dự đoán ra màu sắc
         bright_ness = self.shutter_speed(not self.is_freeze, rays_o) * 2
