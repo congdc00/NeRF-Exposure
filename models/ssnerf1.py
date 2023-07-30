6af371f8436b104f2839141cd48d5a19e9b30dac
CongDC <dinhchicongf9@gmail.com>
2023-07-30 20:56:00 +0700

    :

--- models/ssnerf1.py
+++ models/ssnerf1.py
@@ -100,7 +100,7 @@ class SSNeRF1Model(BaseModel):
 
         density, cor_feature = self.geometry(positions) # Dự đoán mật độ thể tích => density [N_rays];cor_feature [N_rays, 16]16 là số chiều được mã hoá ra
         
-        # self.iterator += 1
+        self.iterator += 1
         if self.iterator > 20000 and self.iterator % 500 == 0:
             self.is_freeze = not self.is_freeze
             rgb = self.texture(self.is_freeze, cor_feature, t_dirs) # Dự đoán ra màu sắc
