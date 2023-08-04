98afbd84b8601da523cee20c7c91b91b1d313a5d
CongDC <dinhchicongf9@gmail.com>
2023-08-04 09:28:38 +0700

    fix

--- systems/ssnerf1.py
+++ systems/ssnerf1.py
@@ -275,8 +275,8 @@ class SSNeRF1System(BaseSystem):
                 delta_exposure_std = torch.std(list_delta_exposure)
                 
                 log_text = f"Validation on {num_imgs}/{num_all_imgs} images -- std PSNR: {psnr_standard} -- SSIM {ssim_score} -- std SSIM: {ssim_standard} -- std Exposure: {round( delta_exposure_std.item(), 3)} -- mean Exposure {mean_exposure}"
-                for key, value in check_ssim.items():
-                     print(f"Name dataset: {key} \t SSIM: {value}")
+                # for key, value in check_ssim.items():
+                #      print(f"Name dataset: {key} \t SSIM: {value}")
 
                 if num_imgs<num_all_imgs:
                     logger.warning(log_text)
