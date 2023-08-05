ba270837981dd11f88b193e18a24daa3c01b77a4
CongDC <dinhchicongf9@gmail.com>
2023-08-05 23:06:49 +0700

    fix

--- models/eneus.py
+++ models/eneus.py
@@ -257,6 +257,7 @@ class ENeuSModel(BaseModel):
         self.is_freeze = not self.is_freeze
         rgb = self.texture(self.is_freeze, feature, t_dirs, normal) # Dự đoán ra màu sắc
         bright_ness = self.shutter_speed(not self.is_freeze, rays_o) * 2
+        
         # print(f"----------------bright_ness {bright_ness[0].item()} --------------------")
         weights = render_weight_from_alpha(alpha, ray_indices=ray_indices, n_rays=n_rays)
         opacity = accumulate_along_rays(weights, ray_indices, values=None, n_rays=n_rays)
