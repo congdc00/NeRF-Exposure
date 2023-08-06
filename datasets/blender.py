f7b2df24a404d1052b27b2a0cca31c65404367f9
CongDC <dinhchicongf9@gmail.com>
2023-08-07 00:38:43 +0700

    fix

--- datasets/blender.py
+++ datasets/blender.py
@@ -71,7 +71,7 @@ class BlenderDatasetBase():
             ## Dang png 2
             
             img = Image.open(img_path)
-            print(f"so kenh mau {img.mode}")
+            print(f"so kenh mau {len(img.getbands())}")
             img.save('test_img.png')
             try:
                 # print(f"Thay đổi độ sáng ảnh từ 1 -> {frame['factor']}")
