50232a0cd51fc66943e9a3e9c7450fdee88457ce
CongDC <dinhchicongf9@gmail.com>
2023-08-12 09:00:39 +0700

    fix

--- systems/ssnerf1.py
+++ systems/ssnerf1.py
@@ -197,8 +197,7 @@ class SSNeRF1System(BaseSystem):
         ##  SSIM
         image_array1 = color_predict.view(H, W, 3).cpu().numpy()
         image_array2 = image_origin.view(H, W, 3).cpu().numpy()
-        ssim = 1
-        # ssim = self.criterions['ssim'](image_array1, image_array2,multichannel=True, full=True)
+        ssim = self.criterions['ssim'](image_array1, image_array2,multichannel=True, full=True)
 
         # mask_object = batch['fg_mask'].view(-1, 1)
         # rgb_non_bg= (batch['rgb']*mask_object)
