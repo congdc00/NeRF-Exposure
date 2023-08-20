b5e558f740e8780291dc6f7b26ca40fc8b433aed
CongDC <dinhchicongf9@gmail.com>
2023-08-21 00:22:32 +0700

    fix

--- systems/nerf.py
+++ systems/nerf.py
@@ -74,7 +74,7 @@ class NeRFSystem(BaseSystem):
             rays_o, rays_d = get_rays(directions, c2w)
 
             #them cho colmap
-            rgb = self.dataset.all_images[index.to(self.rank)]
+            rgb = self.dataset.all_images[index.to('cpu')]
             rgb = rgb.view(-1, self.dataset.all_images.shape[-1])
             rgb = rgb.to(self.rank)
             fg_mask = self.dataset.all_fg_masks[index].view(-1).to(self.rank)
