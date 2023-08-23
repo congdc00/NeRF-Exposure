607816736695ba154fc82aee74605d0b5ca84c4e
congdinhchi <dinhchicongf9@gmail.com>
2023-08-23 14:31:39 +0700

    :

--- systems/ssnerf1.py
+++ systems/ssnerf1.py
@@ -147,7 +147,7 @@ class SSNeRF1System(BaseSystem):
 
         # Total loss
         alpha = 0.01
-        beta = 0.001
+        beta = 0.00001
         total_loss = loss_rgb + alpha*loss_e1 + beta*loss_e2
         self.log('train/loss_rgb', total_loss)
         loss = 0.
