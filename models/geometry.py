d22465462fa5aad921b0ce0940ca0c69ee4f6868
congdinhchi <dinhchicongf9@gmail.com>
2023-08-23 15:36:35 +0700

    :

--- models/geometry.py
+++ models/geometry.py
@@ -121,7 +121,7 @@ class VolumeDensity(BaseImplicitGeometry):
         self.n_output_dims = self.config.feature_dim
         self.encoding_with_network = get_encoding_with_network(self.n_input_dims, self.n_output_dims, self.config.xyz_encoding_config, self.config.mlp_network_config)
 
-    def forward(self,is_freeze, points):
+    def forward(self, points, is_freeze = True):
         # freeze
         for param in  self.encoding_with_network.parameters():
             param.requires_grad = is_freeze
