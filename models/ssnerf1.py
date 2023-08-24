d83a06d3eaa47a5cae38b9d5cac60b577dfc604a
congdinhchi <dinhchicongf9@gmail.com>
2023-08-24 16:19:41 +0700

    :

--- models/ssnerf1.py
+++ models/ssnerf1.py
@@ -108,7 +108,7 @@ class SSNeRF1Model(BaseModel):
         #     rgb = self.texture(self.is_freeze, cor_feature, t_dirs) # Dự đoán ra màu sắc
         #     bright_ness = self.shutter_speed(self.is_freeze, rays_o) * 2
 
-        if self.epoch > 10000:
+        if self.epoch > 1000:
             self.is_true = not self.is_true
         density, cor_feature = self.geometry(positions, self.is_freeze) # Dự đoán mật độ thể tích => density [N_rays];cor_feature [N_rays, 16]16 là số chiều được mã hoá ra
         rgb = self.texture(self.is_freeze, cor_feature, t_dirs) # Dự đoán ra màu sắc
