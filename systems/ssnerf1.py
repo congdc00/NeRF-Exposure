2e8df76c526323a961ca8adcd9f6ee49c6083317
congdinhchi <dinhchicongf9@gmail.com>
2023-08-24 16:49:28 +0700

    :

--- systems/ssnerf1.py
+++ systems/ssnerf1.py
@@ -151,8 +151,8 @@ class SSNeRF1System(BaseSystem):
         # Total loss
         if self.epoch > 1000:
             self.is_true = not self.is_true
-        alpha = 0.00001
-        beta = 0.00001
+        alpha = 0.0001
+        beta = 0.0001
         
         
         if self.is_true:
