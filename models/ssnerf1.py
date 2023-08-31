36d6dd9ff93e0d8e76ee29825feb051e50f08479
congdinhchi <dinhchicongf9@gmail.com>
2023-08-31 16:06:18 +0700

    :

--- models/ssnerf1.py
+++ models/ssnerf1.py
@@ -117,7 +117,7 @@ class SSNeRF1Model(BaseModel):
         
         if self.epoch <= 200:
             
-            bright_ness = torch.full_like([density.size(), 1], 1.0)
+            bright_ness = torch.full_like([density.size()[0], 1], 1.0)
         else:
             bright_ness = self.shutter_speed(not self.is_freeze, rays_o)
 
