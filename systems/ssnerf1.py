0f6a0cc7d954153ce2d1fd19d803f05e4e0ccd19
congdinhchi <dinhchicongf9@gmail.com>
2023-08-31 23:38:51 +0700

    :

--- systems/ssnerf1.py
+++ systems/ssnerf1.py
@@ -149,7 +149,7 @@ class SSNeRF1System(BaseSystem):
         loss_e2 = torch.exp(loss_e2)
 
         # Total loss
-        if self.epoch > 4000:
+        if self.epoch > 24000:
             self.is_true = not self.is_true
         
         
