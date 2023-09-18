9fab4f7c90621274e3d7d90529cb1a86fccb34c9
CongDC <62150684+congdinhchi@users.noreply.github.com>
2023-09-18 18:08:44 +0000

    :

--- models/ssnerf1.py
+++ models/ssnerf1.py
@@ -115,10 +115,10 @@ class SSNeRF1Model(BaseModel):
         rgb = self.texture(self.is_freeze, cor_feature, t_dirs) # Dự đoán ra màu sắc
         bright_ness = self.shutter_speed(not self.is_freeze, rays_o) * 2
 
-        if self.epoch > 10000:
-            self.is_freeze = not self.is_freeze
-        else:
-            bright_ness = torch.full_like(bright_ness, 1.0)
+        # if self.epoch > 10000:
+        #     self.is_freeze = not self.is_freeze
+        # else:
+        #     bright_ness = torch.full_like(bright_ness, 1.0)
             
 
         # network_inp torch.Size([97790, 32])
