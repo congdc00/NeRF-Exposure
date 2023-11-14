ee9732a6f32e5e4722a9b428a4894c2e7a0742a8
CongDC <dinhchicongf9@gmail.com>
2023-11-14 22:36:25 +0700

    :

--- systems/base.py
+++ systems/base.py
@@ -52,11 +52,13 @@ class BaseSystem(pl.LightningModule, SaverMixin):
     But on_after_batch_transfer does not support DP.
     """
     def on_train_batch_start(self, batch, batch_idx, unused=0):
+        print(f"on train batch start")
         self.dataset = self.trainer.datamodule.train_dataloader().dataset
         self.preprocess_data(batch, 'train')
         update_module_step(self.model, self.current_epoch, self.global_step)
     
     def on_validation_batch_start(self, batch, batch_idx, dataloader_idx):
+        print(f"on val batch start")
         self.dataset = self.trainer.datamodule.val_dataloader().dataset
         self.preprocess_data(batch, 'validation')
         update_module_step(self.model, self.current_epoch, self.global_step)
