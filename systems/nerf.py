2a163cd6aaf54bb43ea94ca6c801c76ba18a2f65
CongDC <dinhchicongf9@gmail.com>
2023-11-15 01:19:28 +0700

    :

--- systems/nerf.py
+++ systems/nerf.py
@@ -173,8 +173,8 @@ class NeRFSystem(BaseSystem):
             }
         
         image_origin = batch['rgb'] 
-        print(f"img origin {image_origin.view(H, W, 3).shape}")
-        cv2.imwrite("test.png", batch['rgb'].view(H, W, 3))
+        print(f"img origin {image_origin.view(H, W, 3).numpy().shape}")
+        cv2.imwrite("test.png", batch['rgb'].view(H, W, 3).numpy())
         image_predict = out['comp_rgb']
 
         print(f"image_predict.shape {image_predict.shape}")
