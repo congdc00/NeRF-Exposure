d75efa5b07fe584ed83166751587967f96176b1e
CongDC <62150684+congdc00@users.noreply.github.com>
2023-12-01 14:47:53 +0700

    Update ssnerf1.py

--- models/ssnerf1.py
+++ models/ssnerf1.py
@@ -109,7 +109,7 @@ class SSNeRF1Model(BaseModel):
         if MODE == 1:
             self.is_freeze = not self.is_freeze
         elif MODE == 2:
-            if self.epoch > 25001:
+            if self.epoch > 25000:
                 self.is_freeze = not self.is_freeze
             else:
                 bright_ness = torch.full_like(bright_ness, 1.0)
