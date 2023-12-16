6afd3ff2fc1ebf31b9356f341b28b4ef1e1fd53c
CongDC <dinhchicongf9@gmail.com>
2023-12-16 11:09:17 +0700

    :

--- models/geometry.py
+++ models/geometry.py
@@ -122,10 +122,8 @@ class VolumeDensity(BaseImplicitGeometry):
         self.encoding_with_network = get_encoding_with_network(self.n_input_dims, self.n_output_dims, self.config.xyz_encoding_config, self.config.mlp_network_config)
 
     def forward(self,is_freeze, points):
-        print(f"run 2")
-        if is_freeze:
-            for param in self.encoding_with_network.parameters():
-                param.requires_grad = is_freeze
+        for param in self.encoding_with_network.parameters():
+            param.requires_grad = is_freeze
         points = contract_to_unisphere(points, self.radius, self.contraction_type)
         out = self.encoding_with_network(points.view(-1, self.n_input_dims)).view(*points.shape[:-1], self.n_output_dims).float()
         density, feature = out[...,0], out
