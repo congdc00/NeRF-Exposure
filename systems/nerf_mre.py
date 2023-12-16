d59b515565592aa06c4188b7ac60e4c99bf18721
CongDC <dinhchicongf9@gmail.com>
2023-12-16 12:30:30 +0700

    :

--- systems/nerf_mre.py
+++ systems/nerf_mre.py
@@ -182,11 +182,11 @@ class NeRFMRESystem(BaseSystem):
 
         ex_predict = torch.tensor(list(out['list_ex'].values())).to(device)
         mean_exposure_predict = torch.mean(ex_predict).to(device)
-        print(f" mean_exposure_predict {mean_exposure_predict}")
         
         # loss diff mean exposure with 1
         loss_mean_exposure = torch.pow(mean_exposure_predict - 1, 2)
         
+        print(f" loss_exposure_predict {loss_exposure_predict}")
         # loss diff exposure  
         loss_diff_exposure = ex_predict/mean_exposure_predict-1
         loss_diff_exposure = torch.mean(torch.abs(loss_diff_exposure))
